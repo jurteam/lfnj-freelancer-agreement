d9d1c0316849aee733953651b13ac52359c5757c
tyagishuchi1 <shuchi@jur.io>
2020-03-13 16:41:10 +0530

    updated: naming, add party details

--- contracts/FreelancerAgreement.sol
+++ contracts/FreelancerAgreement.sol
@@ -1,7 +1,6 @@
 pragma solidity >=0.4.21 <0.7.0;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract FreelancerAgreement {
     using SafeMath for uint;
@@ -11,14 +10,15 @@ contract FreelancerAgreement {
 
     enum Roles {Customer, Freelancer}
 
-    uint private MAX_DELIVERY_DEADLINE; //3 months
-    uint private CONTRACT_WARRANTY; //2 years
-    uint private CONTRACT_VALIDITY; //5 years
-    uint public CONTRACT_WARRANTY_EXPIRATION;
-    uint public CONTRACT_VALIDITY_EXPIRATION;
-    uint public DELIVERY_DEADLINE;
-    uint public LATE_DELIVERY_PENALTY;
-    uint public PAYOUT;
+    uint private maxDeliveryDeadline; //3 months
+    uint private contractWarranty; //2 years
+    uint private contractValidity; //5 years
+    uint public contractWarrantyExpiration;
+    uint public contractValidityExpiration;
+    uint public deliveryDeadline;
+    uint public lateDeliveryPenalty;
+    uint public payout;
+    uint private secondsInADay = 86400;
 
     //Token for escrow & voting
     ERC20 public jurToken;
@@ -48,6 +48,10 @@ contract FreelancerAgreement {
 
     event LicenseAdded(string _name, address _owner);
     event PaymentReleased(uint _payout, uint _timestamp);
+    event ContractCreated(bytes32 _agreementHash);
+    event ContractSigned(address _signer);
+    event ProjectDelievered();
+    event StateChange(State state, State _state, uint _timestamp);
 
 
     modifier hasState(State _state) {
@@ -58,19 +62,22 @@ contract FreelancerAgreement {
     modifier isParty(address _sender) {
         require(_sender != address(0), "Address is not a party.");
         require(_sender == customer.partyAddress || _sender == freelancer.partyAddress, "Address is not a party.");
+        _;
     }
 
     modifier isCustomer(address _sender) {
         require(_sender != address(0), "Address is not a party.");
         require(_sender == customer.partyAddress, "Address is not a party.");
+        _;
     }
-
+    //All are in days.
     constructor(
         address _jurToken,
         bytes32 _agreementHash,
         uint _submittionDeadline,
         uint _contractValidity,
         uint _contractWarranty,
+        uint _lateDeliveryPenalty,
         uint _totalPayout
     )
         public
@@ -79,41 +86,50 @@ contract FreelancerAgreement {
         jurToken = ERC20(_jurToken);
         agreementHash = _agreementHash;
 
-        MAX_WORK_SUBMIT_DEADLINE = _submittionDeadline;
-        CONTRACT_WARRANTY = _contractWarranty;
-        CONTRACT_VALIDITY = _contractValidity;
-        LATE_DELIVERY_PENALTY = _lateDeliveryPenalty;
-        PAYOUT = _totalPayout;
+        maxDeliveryDeadline = _submittionDeadline;
+        contractWarranty = _contractWarranty;
+        contractValidity = _contractValidity;
+        lateDeliveryPenalty = _lateDeliveryPenalty;
+        payout = _totalPayout;
 
         emit ContractCreated(_agreementHash);
     }
 
-    function addPartyDetails(
-        Role _role,
+    function addCustomerDetails(
         address _partyAddress,
-        string _name,
-        string _countryOfOperation,
-        string _postalAddress,
+        string memory _name,
+        string memory _countryOfOperation,
+        string memory _postalAddress,
         uint _fiscalIdentificationNumber
     )
         public
-        onlyOwner
         hasState(State.Unsigned)
     {
         require(_partyAddress != address(0), "Address cannot be empty");
-        Party storage _party = Party(_partyAddress, Roles.Customer, _name, _countryOfOperation, _postalAddress, _fiscalIdentificationNumber);
-        if(_role == Roles.Customer) {
-            customer = _party;
-        } else if(_role == Roles.Freelancer) {
-            freelancer = _party;
-        }
+        require(customer.partyAddress == address(0), "Details have already been set");
+        customer = Party(_partyAddress, Roles.Customer, _name, _countryOfOperation, _postalAddress, _fiscalIdentificationNumber);
+    }
+
+    function addFreelancerDetails(
+        address _partyAddress,
+        string memory _name,
+        string memory _countryOfOperation,
+        string memory _postalAddress,
+        uint _fiscalIdentificationNumber
+    )
+        public
+        hasState(State.Unsigned)
+    {
+        require(_partyAddress != address(0), "Address cannot be empty");
+        require(freelancer.partyAddress == address(0), "Details have already been set");
+        customer = Party(_partyAddress, Roles.Freelancer, _name, _countryOfOperation, _postalAddress, _fiscalIdentificationNumber);
     }
 
     function signAgreement() public hasState(State.Unsigned) isParty(msg.sender) {
         require(!hasSigned[msg.sender], "This address has already signed.");
         hasSigned[msg.sender] = true;
         if(msg.sender == customer.partyAddress){
-            require(jurToken.transferFrom(msg.sender, address(this), PAYOUT), "Could not transfer the tokens.");
+            require(jurToken.transferFrom(msg.sender, address(this), payout), "Could not transfer the tokens.");
         }
         bool allSigned = false;
 
@@ -121,28 +137,28 @@ contract FreelancerAgreement {
 
         if (allSigned) {
             setState(State.Signed);
-            CONTRACT_VALIDITY_EXPIRATION = SafeMath.add(getNow, CONTRACT_VALIDITY);
-            CONTRACT_WARRANTY_EXPIRATION = SafeMath.add(getNow, CONTRACT_WARRANTY);
-            DELIVERY_DEADLINE = SafeMath.add(getNow, MAX_DELIVERY_DEADLINE);
+            contractValidityExpiration = SafeMath.add(getNow(), contractValidity * 1 days);
+            contractWarrantyExpiration = SafeMath.add(getNow(), contractWarranty * 1 days);
+            deliveryDeadline = SafeMath.add(getNow(), maxDeliveryDeadline * 1 days);
         }
         emit ContractSigned(msg.sender);
     }
 
     function markProjectComplete() public isParty(msg.sender) {
         setState(State.Delivered);
-        if(getNow() > DELIVERY_DEADLINE) {
+        if(getNow() > deliveryDeadline) {
             //TODO divide this by number of miliseconds in a day.
-            uint daysExtended = SafeMath.sub(getNow(), DELIVERY_DEADLINE);
-            uint dailyPenalty = SafeMath.div(SafeMath.mul(LATE_DELIVERY_PENALTY, PAYOUT), 100);
-            PAYOUT = SafeMath.sub(PAYOUT, SafeMath.mul(dailyPenalty, daysExtended));
+            uint daysExtended = SafeMath.sub(getNow(), deliveryDeadline) / secondsInADay;
+            uint dailyPenalty = SafeMath.div(SafeMath.mul(lateDeliveryPenalty, payout), 100);
+            payout = SafeMath.sub(payout, SafeMath.mul(dailyPenalty, daysExtended));
         }
         emit ProjectDelievered();
     }
 
     function releasePayout() public isCustomer(msg.sender) hasState(State.Delivered) {
         setState(State.Completed);
-        require(jurToken.transfer(freelancer.partyAddress, PAYOUT), "Could not transfer funds.");
-        emit PaymentReleased(PAYOUT, getNow());
+        require(jurToken.transfer(freelancer.partyAddress, payout), "Could not transfer funds.");
+        emit PaymentReleased(payout, getNow());
     }
 
     function setState(State _state) internal {
@@ -150,8 +166,8 @@ contract FreelancerAgreement {
         state = _state;
     }
 
-    function uploadLicence(bytes32 _licenseHash, string _name) public isParty(msg.sender) {
-        licenses[licenseCount++] = License(_licenseHash, _name, msg.sender);
+    function uploadLicence(bytes32 _licenseHash, string memory _name) public isParty(msg.sender) {
+        licences[licenseCount++] = License(_licenseHash, _name, msg.sender);
         emit LicenseAdded(_name, msg.sender);
     }
 
